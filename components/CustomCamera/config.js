e1a0d89a5c529fcbc26809190421f03f900213ca
La Tiao Di <2322743837@qq.com>
2024-11-30 11:24:53 -0800

    更新 .gitignore 文件，移除多余包文件

---
+++ components/CustomCamera/config.js
@@ -0,0 +1,5 @@
+export default {
+	side: '/static/masking/side.jpg', //身份证国徽面
+	front: '/static/masking/front.jpg', //身份证正面
+	portrait:'/static/masking/portrait.jpg', //半身照
+}
---
+++ unpackage/dist/build/mp-weixin/uni_modules/uni-nav-bar/components/uni-nav-bar/uni-status-bar.js
@@ -0,0 +1,10 @@
+(global["webpackJsonp"]=global["webpackJsonp"]||[]).push([["uni_modules/uni-nav-bar/components/uni-nav-bar/uni-status-bar"],{"0d64":function(n,t,u){"use strict";(function(n){Object.defineProperty(t,"__esModule",{value:!0}),t.default=void 0;var u={name:"UniStatusBar",data:function(){return{statusBarHeight:n.getSystemInfoSync().statusBarHeight+"px"}}};t.default=u}).call(this,u("df3c")["default"])},"1fce":function(n,t,u){"use strict";var a=u("8634"),e=u.n(a);e.a},8634:function(n,t,u){},9367:function(n,t,u){"use strict";u.r(t);var a=u("0d64"),e=u.n(a);for(var i in a)["default"].indexOf(i)<0&&function(n){u.d(t,n,(function(){return a[n]}))}(i);t["default"]=e.a},a9d1:function(n,t,u){"use strict";u.d(t,"b",(function(){return a})),u.d(t,"c",(function(){return e})),u.d(t,"a",(function(){}));var a=function(){var n=this.$createElement;this._self._c},e=[]},ef73:function(n,t,u){"use strict";u.r(t);var a=u("a9d1"),e=u("9367");for(var i in e)["default"].indexOf(i)<0&&function(n){u.d(t,n,(function(){return e[n]}))}(i);u("1fce");var r=u("828b"),c=Object(r["a"])(e["default"],a["b"],a["c"],!1,null,null,null,!1,a["a"],void 0);t["default"]=c.exports}}]);
+;(global["webpackJsonp"] = global["webpackJsonp"] || []).push([
+    'uni_modules/uni-nav-bar/components/uni-nav-bar/uni-status-bar-create-component',
+    {
+        'uni_modules/uni-nav-bar/components/uni-nav-bar/uni-status-bar-create-component':(function(module, exports, __webpack_require__){
+            __webpack_require__('df3c')['createComponent'](__webpack_require__("ef73"))
+        })
+    },
+    [['uni_modules/uni-nav-bar/components/uni-nav-bar/uni-status-bar-create-component']]
+]);
